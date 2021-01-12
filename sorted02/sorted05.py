d1f8e6529253c6f4aab5a8e431e35387ac10d7c5
bradapratt <bradapratt@gmail.com>
2021-01-12 13:46:06 -0800

    Completed lab 32

---
+++ sorted02/sorted05.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+us_invasion = [{'ip':'10.10.1.2', 'un':'john', 'pw':'allstar'}, \
+{'ip':'10.10.1.3', 'un':'paul', 'pw':'iils20s3'}, \
+{'ip':'10.10.1.4', 'un':'george', 'pw':'hunkydoryzory'}, \
+{'ip':'10.10.1.5', 'un':'stuart', 'pw':'alta3'}, \
+{'ip':'10.10.1.6', 'un':'pete', 'pw':'a8dd827z3'}]
+
+def byUserName(x):
+    return x['un']
+
+listbyusername = sorted(us_invasion, key=byUserName)
+print("\nThe list us_invasion looks like: ", us_invasion)
+print("\nResult of sorted(us_invasion, key=byUserName): ", listbyusername)
+
+def byIP(x):
+    return x['ip']
+
+listbyIP = sorted(us_invasion, key=byIP)
+print("Result of sorted(us_invasion, key=byIP): ", listbyIP)
